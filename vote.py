4af2f116ba8bf707f56832d20ec79e90b348ff73
Christopher Sherlock <chris.sherlock79@gmail.com>
2025-11-09 18:29:45 +1100

    make the program work! also check to make sure that the voter hasn't voted

--- vote.py
+++ vote.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
-from verify import *
+from src.verify import *
+from src.voting import *
 
 # ask for a voter id until a blank is entered
 
+voters = []
 voterid = input("Please enter Voter ID: ")
 
 while voterid != "":
     if not is_valid_voterid(voterid):
         print("Invalid Voter ID\n")
+
+    if has_already_voted(voters, voterid):
+        print("\nYou have already voted in this election. You cannot vote again.\n")
+        
+    voters.append(voterid)
 
     voterid = input("Please enter Voter ID: ")
