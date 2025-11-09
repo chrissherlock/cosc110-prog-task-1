720dfcae6e074ee693be22d1a30695943050bc5d
Christopher Sherlock <chris.sherlock79@gmail.com>
2025-11-09 21:29:45 +1100

    add preconditions

--- src/verify.py
+++ src/verify.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
-def is_valid_voterid(voter_id) -> bool:
+def is_valid_voterid(voter_id: str) -> bool:
     """checks the voter id is 7 numeric digits"""
+
+    assert(isinstance(voter_id, str))
+
     return voter_id.isdigit() and int(voter_id) >= 1000000 and int(voter_id) <= 9999999
 
-def is_valid_vote(vote) -> bool:
+def is_valid_vote(vote: str) -> bool:
     """checks the vote is between 0 and 9"""
+
+    assert(isinstance(vote, str))
+
     return vote.isdigit() and int(vote) >= 0 and int(vote) <= 9
