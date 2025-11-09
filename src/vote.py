59557ef62c823dc023a4cab7e8b456adabaa74a6
Christopher Sherlock <chris.sherlock79@gmail.com>
2025-11-09 16:56:00 +1100

    keep asking for a voter id until a blank is entered

--- src/vote.py
+++ src/vote.py
@@ -2,6 +2,12 @@
 
 from verify import *
 
+# ask for a voter id until a blank is entered
+
 voterid = input("Please enter Voter ID: ")
-if not is_valid_voterid(voterid):
-    print("Invalid Voter ID\n")
+
+while voterid != "":
+    if not is_valid_voterid(voterid):
+        print("Invalid Voter ID\n")
+
+    voterid = input("Please enter Voter ID: ")
