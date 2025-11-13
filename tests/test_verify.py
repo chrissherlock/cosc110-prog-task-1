b3ef220e54f7ed8cff80b5869168fd370fa5a93f
Christopher Sherlock <chris.sherlock79@gmail.com>
2025-11-14 00:24:41 +1100

    move test to correct file

--- tests/test_verify.py
+++ tests/test_verify.py
@@ -38,5 +38,12 @@ class TestVerifications(unittest.TestCase):
         self.assertFalse(is_valid_vote("10"))
         self.assertFalse(is_valid_vote(""))
 
+    def test_has_aleady_voted(self):
+        """test to see if the voter has already voted"""
+        voters = ["1111111", "1234567"]
+
+        self.assertTrue(has_already_voted(voters, "1234567"))
+        self.assertFalse(has_already_voted(voters, "7654321"))
+
 if __name__ == '__main__':
     unittest.main()
--- tests/test_voting.py
+++ tests/test_voting.py
@@ -9,13 +9,6 @@ from src.voting import *
 class TestVoting(unittest.TestCase):
     """Test suite for voting module"""
 
-    def test_has_aleady_voted(self):
-        """test to see if the voter has already voted"""
-        voters = ["1111111", "1234567"]
-
-        self.assertTrue(has_already_voted(voters, "1234567"))
-        self.assertFalse(has_already_voted(voters, "7654321"))
-
     def test_prompt_text_for_candidate(self):
         """test to ensure that candidate prompt is correct"""
         self.assertEqual("Please enter an integer score for William Gorithm (0 is worst, 9 is best): ", prompt_text_for_candidate("William Gorithm"))
