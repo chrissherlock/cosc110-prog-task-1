76d0478346a37242425ce784987bdd9e80b5c05f
Christopher Sherlock <chris.sherlock79@gmail.com>
2025-11-13 22:37:42 +1100

    output results in own function

--- voting.py
+++ voting.py
@@ -2,15 +2,15 @@
 
 from src.voting import *
 
-tally = tally_candidates(get_votes())
-winner = determine_winner(tally)
+def print_results(tally: dict):
+    """Output the results of the election, with the average votes for each candidate."""
+    winner = determine_winner(tally)
 
-# output the results of the election, with the average votes for each candidate,
-# and the name of the actual winner
+    print("\nResults\n")
 
-print("\nResults\n")
+    for candidate in tally:
+        print(f"{candidate}: {tally[candidate]}")
 
-for candidate in tally:
-    print(f"{candidate}: {tally[candidate]}")
+    print(f"{winner} wins with the average score {tally[winner]}!\n")
 
-print(f"{winner} wins with the average score {tally[winner]}!\n")
+print_results(tally_candidates(get_votes()))
