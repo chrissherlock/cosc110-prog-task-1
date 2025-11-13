920d2b109b11af76653442cd9f0df47138ecef1a
Christopher Sherlock <chris.sherlock79@gmail.com>
2025-11-13 22:32:02 +1100

    move functions to voting.py in the module

--- src/voting.py
+++ src/voting.py
@@ -106,3 +106,49 @@ def determine_winner(tally: dict) -> str:
         return "Meg A. Byte"
 
     return "Oliver Seton"
+
+def get_votes() -> dict:
+    """Ask for a voter id until a blank is entered."""
+    voters = []
+
+    candidates = {
+        "William Gorithm": [],
+        "Meg A. Byte": [],
+        "Oliver Seton": []
+    }
+
+    while True:
+        voterid = input("Please enter Voter ID: ")
+
+        if voterid == '':
+            break
+
+        if not is_valid_voterid(voterid):
+            print("Invalid Voter ID\n")
+            continue;
+
+        if has_already_voted(voters, voterid):
+            print("You have already voted in this election. You cannot vote again.\n")
+            continue;
+            
+        voters.append(voterid)
+
+        for candidate in candidates:
+            vote_pref = input(prompt_text_for_candidate(candidate))
+
+            while not is_valid_vote(vote_pref):
+                print("Please enter an integer score between 0 and 9")
+                vote_pref = input(prompt_text_for_candidate(candidate))
+
+            candidates[candidate].append(int(vote_pref))
+
+    return candidates
+
+def tally_candidates(candidates: dict) -> dict:
+    """Get the tally of all the votes for the candidates and determine the winner"""
+    tally = {}
+
+    for candidate in candidates:
+        tally[candidate] = get_candidate_average_votes(candidates[candidate])
+
+    return tally
--- voting.py
+++ voting.py
@@ -1,53 +1,6 @@
 #!/usr/bin/env python3
 
-from src.verify import *
 from src.voting import *
-
-def get_votes() -> dict:
-    """Ask for a voter id until a blank is entered."""
-    voters = []
-
-    candidates = {
-        "William Gorithm": [],
-        "Meg A. Byte": [],
-        "Oliver Seton": []
-    }
-
-    while True:
-        voterid = input("Please enter Voter ID: ")
-
-        if voterid == '':
-            break
-
-        if not is_valid_voterid(voterid):
-            print("Invalid Voter ID\n")
-            continue;
-
-        if has_already_voted(voters, voterid):
-            print("You have already voted in this election. You cannot vote again.\n")
-            continue;
-            
-        voters.append(voterid)
-
-        for candidate in candidates:
-            vote_pref = input(prompt_text_for_candidate(candidate))
-
-            while not is_valid_vote(vote_pref):
-                print("Please enter an integer score between 0 and 9")
-                vote_pref = input(prompt_text_for_candidate(candidate))
-
-            candidates[candidate].append(int(vote_pref))
-
-    return candidates
-
-def tally_candidates(candidates: dict) -> dict:
-    """Get the tally of all the votes for the candidates and determine the winner"""
-    tally = {}
-
-    for candidate in candidates:
-        tally[candidate] = get_candidate_average_votes(candidates[candidate])
-
-    return tally
 
 tally = tally_candidates(get_votes())
 winner = determine_winner(tally)
